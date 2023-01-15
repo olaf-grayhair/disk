73eeee45c1de0442a279fa5040b550890a3cd1ba
olaf-grayhair <coldworld77777@gmail.com>
2023-01-15 19:45:06 +0100

    https

--- client/src/utils/instance.js
+++ client/src/utils/instance.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export const instance = axios.create(
   {
-    baseURL: 'http://disk.oleh-oskin.shop/',
+    baseURL: 'https://disk.oleh-oskin.shop/',
     // baseURL: 'http://localhost:3004/',
     headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
   })
 
-export const baseURL = 'http://disk.oleh-oskin.shop/'
+export const baseURL = 'https://disk.oleh-oskin.shop/'
