c234b063ef2b6fb3cb0904f410da14b20630afb1
olaf-grayhair <coldworld77777@gmail.com>
2023-01-21 18:39:29 +0100

    repair instance

--- client/src/utils/instance.js
+++ client/src/utils/instance.js
@@ -10,10 +10,10 @@ export const setAuthToken = token => {
 
 export const instance = axios.create(
   {
-    // baseURL: 'https://disk.oleh-oskin.shop/',
-    baseURL: 'http://localhost:3004/',
+    baseURL: 'https://disk.oleh-oskin.shop/',
+    // baseURL: 'http://localhost:3004/',
     headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
   })
 
-export const baseURL = 'http://localhost:3004/'
-// export const baseURL = 'https://disk.oleh-oskin.shop/'
+// export const baseURL = 'http://localhost:3004/'
+export const baseURL = 'https://disk.oleh-oskin.shop/'
